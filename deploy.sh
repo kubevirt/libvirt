6042aa00f8a53e13bf908da76a8d7d0d504ca655
Roman Mohr <rmohr@redhat.com>
2019-09-18 17:11:21 +0200

    Take release of rpm into account when pushing
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-export LIBVIRT_VERSION=$(docker run --rm -it kubevirt/libvirt libvirtd --version | awk -F' ' '{print $NF}' | tr -d '[:space:]' )
+export LIBVIRT_VERSION=$(docker run --rm -it kubevirt/libvirt rpm -q --qf "%{VERSION}-%{RELEASE}\n" libvirt-daemon | tr -d '[:space:]')
 docker tag kubevirt/libvirt:latest kubevirt/libvirt:${LIBVIRT_VERSION}
-docker login -u="${DOCKER_USER}" -p="${DOCKER_PASS}" && docker push kubevirt/libvirt
+docker login -u="${DOCKER_USER}" -p="${DOCKER_PASS}" && docker push kubevirt/libvirt:${LIBVIRT_VERSION}
