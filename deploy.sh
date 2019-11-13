06ee13b3862ade3cba839443de9c4b2e96d771ac
Andrea Bolognani <abologna@redhat.com>
2019-11-13 20:29:45 +0100

    deploy: Change tag format
    Right now we're using the libvirt version as the tag, but that's
    problematic for a couple of reasons: first of all, other information
    such as the QEMU version are not included, which means that even if
    we change the contents of the container significantly, as long as
    we use the same libvirt version, we end up overwriting the same tag
    with every subsequent build; more importantly, it does not include
    the architecture name, so the Travis CI job that takes the longest
    ends up overwriting the tag created by the one that finished first.
    
    The new scheme is completely disconnected from the libvirt version
    and contains enough information to uniquely identify a build: the
    shortened git commit hash, along with the architecture name. In
    addition to that, the day of the last git update is included as
    well, giving images a natural ordering that can easily be taken
    advantage of with tools such as sort(1).
    
    The one scenario where the new versioning scheme partially breaks
    down is when more than one git change is committed per day, but
    that should be rare enough that it doesn't make sense to include
    extra information such as a more detailed timestamp to account
    for it.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ set -xe
 
 source config
 
-docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${LIBVIRT_VERSION}"
+GIT_UNIX_TIMESTAMP=$(git show --format='%ct' | head -1)
+GIT_TIMESTAMP=$(date --date="@${GIT_UNIX_TIMESTAMP}" '+%Y%m%d')
+GIT_COMMIT=$(git describe --always)
+HOST_ARCH=$(uname -m)
+TAG="${GIT_TIMESTAMP}.${GIT_COMMIT}"
+
 docker login -u="${DOCKER_USER}" -p="${DOCKER_PASS}"
-docker push "${IMAGE_NAME}:${LIBVIRT_VERSION}"
+
+docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${TAG}.${HOST_ARCH}"
+docker push "${IMAGE_NAME}:${TAG}.${HOST_ARCH}"
