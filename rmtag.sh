9aaa93cf2c4f8d0b06946b5833788d8acc12a438
Andrea Bolognani <abologna@redhat.com>
2019-11-27 16:06:15 +0100

    deploy: Refactor logic
    Up until now we've hardcoded a lot of information regarding what
    architectures we want to build on and which of the corresponding
    workers is tasked with performing additional work; as a result,
    we've made it more difficult to add or (perhaps temporarily)
    remove support for certain architectures.
    
    This commit changes the deploy logic so that updating the list
    of architectures we want to build on requires touching only two
    configuration files and zero code.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- rmtag.sh
+++ rmtag.sh
@@ -8,7 +8,11 @@ source config
 test "$#" -eq 3
 DOCKER_USER="$1"
 DOCKER_PASS="$2"
-TAG="$3"
+IMAGE_AND_TAG="$3"
+
+# We've been passed both the image name and the tag, so we need to
+# extract the latter
+TAG="${IMAGE_AND_TAG##*:}"
 
 # Unfortunately the Docker CLI doesn't expose the ability to delete
 # remote tags, so we have to resort to calling the API directly using
