5710e754f54527cea64ea69eadc4acb6b230bbc9
Andrea Bolognani <abologna@redhat.com>
2019-11-27 16:07:54 +0100

    config: Add COPR_VERSION
    We want to maintain support for multiple package streams in
    parallel, so it makes sense to have the COPR version among all
    other versions we already keep track of.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -xe
 source config
 
 docker build \
+    --build-arg COPR_VERSION="${COPR_VERSION}" \
     --build-arg FEDORA_VERSION="${FEDORA_VERSION}" \
     --build-arg LIBVIRT_VERSION="${LIBVIRT_VERSION}" \
     --build-arg QEMU_VERSION="${QEMU_VERSION}" \
