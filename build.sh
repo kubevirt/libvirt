ada077c24bd1367c953d641336d195f55d1cd735
Andrea Bolognani <abologna@redhat.com>
2020-05-19 00:45:41 +0200

    Switch to buildx for building and deploying
    Instead of spawning multiple workers and building containers
    natively on the respective architecture, later coordinating them in
    order to publish a single manifest on Docker Hub, take advantage of
    qemu-static and buildx to generate and test all multi-architecture
    containers on a single amd64 worker.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- build.sh
+++ build.sh
@@ -4,8 +4,38 @@ set -xe
 
 source config
 
-docker build \
-    -t "${IMAGE_NAME}" .
+# We need to build for one architecture at a time because buildx
+# can't currently export multi-architecture containers to the Docker
+# daemon, so we wouldn't be able to test the results otherwise. The
+# deploy step will reuse the cached layers, so we're not wasting any
+# extra time building twice nor are we uploading contents that we
+# haven't made sure actually works
+for ARCH in ${TARGET_ARCHITECTURES}; do
 
-docker run --rm "${IMAGE_NAME}" libvirtd --version
-docker run --rm "${IMAGE_NAME}" "/usr/libexec/qemu-kvm" --version
+    TAG="${ARCH}"
+    DOCKER_PLATFORM="linux/${ARCH}"
+
+    docker buildx build \
+        --load \
+        --platform="${DOCKER_PLATFORM}" \
+        --tag "${IMAGE_NAME}:${TAG}" \
+        .
+
+    docker run \
+        --rm \
+        --platform="${DOCKER_PLATFORM}" \
+        "${IMAGE_NAME}:${TAG}" \
+        uname -m
+    docker run \
+        --rm \
+        --platform="${DOCKER_PLATFORM}" \
+        "${IMAGE_NAME}:${TAG}" \
+        libvirtd --version
+    docker run \
+        --rm \
+        --platform="${DOCKER_PLATFORM}" \
+        "${IMAGE_NAME}:${TAG}" \
+        /usr/libexec/qemu-kvm --version
+
+    docker rmi "${IMAGE_NAME}:${TAG}"
+done
--- deploy.sh
+++ deploy.sh
@@ -7,44 +7,16 @@ source config
 GIT_UNIX_TIMESTAMP=$(git show --format='%ct' | head -1)
 GIT_TIMESTAMP=$(date --date="@${GIT_UNIX_TIMESTAMP}" '+%Y%m%d')
 GIT_COMMIT=$(git describe --always)
-HOST_ARCH=$(uname -m)
 TAG="${COPR_VERSION}-${GIT_TIMESTAMP}-${GIT_COMMIT}"
+DOCKER_PLATFORM="linux/$(echo "${TARGET_ARCHITECTURES}" | sed "s: :,linux/:g")"
 
 docker login -u="${DOCKER_USER}" -p="${DOCKER_PASS}"
 
-docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:tmp.${TAG}.${HOST_ARCH}"
-docker push "${IMAGE_NAME}:tmp.${TAG}.${HOST_ARCH}"
-
-# We need one of the workers to stick around so that it can create
-# and push the manifest, as well as clean up
-if test "${HOST_ARCH}" != "${MAIN_ARCH}"; then
-    exit 0
-fi
-
-TMP_IMAGES=
-for ARCH in ${MAIN_ARCH} ${OTHER_ARCHES}; do
-    TMP_IMAGES="${TMP_IMAGES} ${IMAGE_NAME}:tmp.${TAG}.${ARCH}"
-done
-
-# Wait for the other images to be available before proceeding
-while true; do
-    ALL_DONE=yes
-    for TMP_IMAGE in ${TMP_IMAGES}; do
-        docker pull "${TMP_IMAGE}" || ALL_DONE=no
-    done
-    if test "${ALL_DONE}" = "yes"; then
-        break
-    fi
-    sleep 5
-done
-
-docker manifest create "${IMAGE_NAME}:${TAG}" ${TMP_IMAGES}
-docker manifest push "${IMAGE_NAME}:${TAG}"
-
-docker manifest create "${IMAGE_NAME}:latest" ${TMP_IMAGES}
-docker manifest push "${IMAGE_NAME}:latest"
-
-# Remove architecture-specific tags: we no longer need them
-for TMP_IMAGE in ${TMP_IMAGES}; do
-    bash rmtag.sh "${DOCKER_USER}" "${DOCKER_PASS}" "${TMP_IMAGE}"
-done
+# This builds a multi-architecture image and uploads it, all in a
+# single step. The cached layers from the build step will be reused,
+# so ultimately this only assembles and pushes the manifest
+docker buildx build \
+    --push \
+    --platform="${DOCKER_PLATFORM}" \
+    --tag "${IMAGE_NAME}:${TAG}" \
+    .
