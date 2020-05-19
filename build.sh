487da5fad502226c0e6b9f7be45c0c2767b580ba
Andrea Bolognani <abologna@redhat.com>
2020-05-19 12:44:05 +0200

    prepare: Preprocess Dockerfile
    We need to do this upfront instead of using the built-in ARG
    feature because of current bugs/limitations in buildx.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ set -xe
 source config
 
 docker build \
-    --build-arg COPR_VERSION="${COPR_VERSION}" \
-    --build-arg FEDORA_VERSION="${FEDORA_VERSION}" \
-    --build-arg LIBVIRT_VERSION="${LIBVIRT_VERSION}" \
-    --build-arg QEMU_VERSION="${QEMU_VERSION}" \
     -t "${IMAGE_NAME}" .
 
 docker run --rm "${IMAGE_NAME}" libvirtd --version
