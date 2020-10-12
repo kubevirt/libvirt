9c0267316eb3c4a025ac8dd6b2d5fc8199889e62
Cole Robinson <crobinso@redhat.com>
2020-10-12 14:32:46 -0400

    deploy: Move programmatic env variables to `common.sh`
    These will be needed elsewhere in upcoming patches
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -3,12 +3,7 @@
 set -xe
 
 source hack/config
-
-GIT_UNIX_TIMESTAMP=$(git show --format='%ct' | head -1)
-GIT_TIMESTAMP=$(date --date="@${GIT_UNIX_TIMESTAMP}" '+%Y%m%d')
-GIT_COMMIT=$(git describe --always)
-TAG="${GIT_TIMESTAMP}-${GIT_COMMIT}"
-DOCKER_PLATFORM="linux/$(echo "${TARGET_ARCHITECTURES}" | sed "s: :,linux/:g")"
+source hack/common.sh
 
 docker login -u="${DOCKER_USER}" -p="${DOCKER_PASS}"
 
